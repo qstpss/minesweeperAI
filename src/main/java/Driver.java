5e95859e56a4c24ad08c99230c306fc03892c815
mikhail <m.tkachenko94@gmail.com>
2019-07-14 22:37:24 +0300

    added model, driver singleton

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public enum Driver {
+    INSTANCE;
+
+    private WebDriver driver;
+    public static final String driverPath = "/home/mikhail/Documents/chromedriver";
+
+    void init() {
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        this.driver = new ChromeDriver();
+        driver.get("http://minesweeperonline.com");
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
